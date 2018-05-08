4381b6752724471dd856650ac21212ef1d283d43
artificerpi <11538418+artificerpi@users.noreply.github.com>
2018-05-08 21:48:03 +0800

    fix no default page display

--- websites/SpringBootDemo/src/main/java/com/example/demo/HelloController.java
+++ websites/SpringBootDemo/src/main/java/com/example/demo/HelloController.java
@@ -2,11 +2,12 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	public String hello() {
+	public @ResponseBody String hello() {
 		return "Hello World!";
 	}
 
